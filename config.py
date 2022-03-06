702d886ba431017b547c0734fee9e5df799e0760
ruthjelimo <ruthjmimo@gmail.com>
2022-03-06 12:29:06 +0300

    updated config file

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:ruth@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
 class ProdConfig(Config):
     '''
     Production  configuration child class
