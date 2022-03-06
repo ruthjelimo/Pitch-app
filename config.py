86c61ef53256f5d92bdf8efe8e83229c35b141e8
ruthjelimo <ruthjmimo@gmail.com>
2022-03-06 12:51:51 +0300

    updated start files

--- config.py
+++ config.py
@@ -24,3 +24,10 @@ config_options = {
 'development':DevConfig,
 'production':ProdConfig
 }
+
+# #  email configurations
+#     MAIL_SERVER = 'smtp.googlemail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
