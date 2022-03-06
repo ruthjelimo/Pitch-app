b5ee18761361395d1eb6cc8f7dc84dd1457bd9e8
ruthjelimo <ruthjmimo@gmail.com>
2022-03-06 12:27:35 +0300

    updated files

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app = Flask(__name__)
+   
     # Creating the app configurations
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
@@ -23,6 +24,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+      # configure UploadSet
+    configure_uploads(app,photos)
     # Registering the blueprint
   
     # from .main import main as main_blueprint
