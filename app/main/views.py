05036311d1e673e917047f84bba2eef77db2ae62
ruthjelimo <ruthjmimo@gmail.com>
2022-03-06 11:32:26 +0300

    updated views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Reviews, User
+# from app import auth
 from . import main
 from flask_login import login_required
+from flask_login import login_user,logout_user,login_required
 #views
 @main.route('/')
 def index():
@@ -9,6 +12,20 @@ def index():
     '''
     return render_template('index.html')
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+# @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+# @login_required
+# def new_review(id):
+@auth.route('/logout')
 @login_required
-def new_review(id):
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(255),unique = True,index = True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    password_hash = db.Column(db.String(255))
+    password_secure = db.Column(db.String(255))
     reviews = db.relationship('Comments',backref = 'user',lazy = "dynamic")
     @property
     def password(self):
