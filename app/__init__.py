3f2046189608c43a6bb1b8fb7cac196b72d0094e
ruthjelimo <ruthjmimo@gmail.com>
2022-03-09 15:03:42 +0300

    Deploy

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,8 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 def create_app(config_name):
-    app = Flask(__name__)
+    # app = Flask(__name__)
+    app = Flask()
     # creating the app configurations
     app.config.from_object(config_options[config_name])
     # Initializing flask extensions
